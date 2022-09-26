898cbdb326918bea2455eaa16dce0684dada9133
Jian Luo <13088883724@163.com>
2022-09-26 19:57:59 +0800

    Add files via upload

---
+++ search/insertValueSearch.js
@@ -0,0 +1,21 @@
+function insertValueSearch(arr, left, right, findVal) {
+if (left > right || findVal < arr[0] || findVal > arr[arr.lengh - 1]) {
+        return -1;
+    }
+    let mid = left + (right - left) * Math.floor((findVal - arr[left]) / (arr[right] - arr[left]));
+    let midVal = arr[mid];
+    if (findVal > midVal) {
+        return insertValueSearch(arr, mid + 1, right);
+    } else if (findVal < midVal) {
+        return insertValueSearch(arr, left, mid - 1);
+    } else {
+        return mid;
+    }
+}
+
+let arr = [];
+for (let i = 0; i < 100; i++) {
+    arr.push(i);
+}
+
+console.log(insertValueSearch(arr,0,arr.length-1,99));
